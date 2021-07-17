a96bba6f87798d762e50c05289c7b748f670f636
Jaguar838 <oleg.yamgurov@gmail.com>
2021-07-17 09:01:10 +0300

    settings

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,15 @@
-// import { Route, Switch } from 'react-router-dom';
-// import { NotFoundView } from 'views';
+import { Route, Switch } from 'react-router-dom';
+import { NotFoundView } from 'views';
 // import { ROUTE_PATHS } from 'services/route-paths';
 
 const App = () => {
   return (
     <>
       <Switch>
-        <Route path={ROUTE_PATHS._()} exact component={ContactsView} />
+        {/* <Route path={ROUTE_PATHS._()} exact component={ContactsView} /> */}
         <Route component={NotFoundView} />
       </Switch>
+      <p>Hi!</p>
     </>
   );
 };
