9f2d8a9e5c3289f47f904fec45c2af4a74dda82b
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-19 09:55:57 +0300

    add Filter

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import css from './Filter.module.scss';
+
+const Filter = ({ value, onChange }) => {
+  return (
+    <label className={css.label}>
+      <span className={css.labelText}>Find contacts by name</span>
+      <input
+        type="text"
+        name="name"
+        value={value}
+        onChange={onChange}
+        placeholder="Enter name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+        required
+        className={css.input}
+      />
+    </label>
+  );
+};
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
