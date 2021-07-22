1973e068a2002118784b51de2c2562954e8be6c5
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-22 04:35:48 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,17 +4,12 @@ import {SectionWrap} from 'UI/SectionWrap'
 import { ContactForm } from "components/ContactForm";
 import { ContactList } from 'components/ContactList';
 import { Filter } from 'components/Filter';
-// import { testContacts } from '../../server/contacts.json'
+import testContacts from 'server/contacts.json';
 // console.log(testContacts);
 export default class App extends Component {
   
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', phone: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', phone: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', phone: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', phone: '227-91-26' },
-    ],
+    contacts: {testContacts},
     filter: '',
     name: '',
     phone: ''
