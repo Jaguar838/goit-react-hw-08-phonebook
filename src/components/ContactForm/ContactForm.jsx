6168b11fb8b65f16ae18c16a2460f39366364886
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-08-03 12:54:31 +0300

    Refactoring ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,44 +7,54 @@ const INICIAL_STATE = {
     name: '',
     phone: '',
 };
-export function ContactForm () {
-    static propTypes = {
-        onAdd: PropTypes.func.isRequired,
-    };
-    state = INICIAL_STATE;
+export function ContactForm ({onAdd}) {
+    
+    const [name,setName]=useState('');
+  const [phone,setPhone]=useState('');
 
     function handleChangeForm ({ target })  {
         const { name, value } = target;
-        this.setState({ [name]: value });
+        
+        switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "number":
+        setPhone(value);
+        break;
+      default:
+        return;
+    }
     };
 
     function handleFormSubmit(evt) {
         evt.preventDefault();
-        const { name, phone } = this.state;
-        const { onAdd } = this.props;
-        const isValidateForm = this.validateForm();
-        if (!isValidateForm) return;
+//         const { name, phone } = this.state;
+//         const { onAdd } = this.props;
+//         const isValidateForm = this.validateForm();
+//         if (!isValidateForm) return;
         onAdd({ id: uuid(), name, phone });
-        this.resetForm();
+        resetForm();
     };
 
-    function resetForm = () => {
-        this.setState(INICIAL_STATE);
+    resetForm = () => {
+    setName('');
+    setPhone('');
     };
 
-    function validateForm() {
-        const { name, phone } = this.state;
-        const { onCheckUnique } = this.props;
-        if (!name || !phone) {
-            alert('Some field is empty');
-            return false;
-        }
-        return onCheckUnique(name);
-    };
+//     function validateForm() {
+//         const { name, phone } = this.state;
+//         const { onCheckUnique } = this.props;
+//         if (!name || !phone) {
+//             alert('Some field is empty');
+//             return false;
+//         }
+//         return onCheckUnique(name);
+//     };
     render() {
         const { name, phone } = this.state;
         return (
-            <form onSubmit={this.handleFormSubmit}>
+            <form onSubmit={handleFormSubmit(evt)}>
                 <input
                     className={css.input}
                     type="text"
@@ -53,7 +63,7 @@ export function ContactForm () {
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
                     value={name}
-                    onChange={this.handleChangeForm}
+                    onChange={handleChangeForm(target)}
                 />
                 <input
                     className={css.input}
@@ -63,7 +73,7 @@ export function ContactForm () {
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
                     value={phone}
-                    onChange={this.handleChangeForm}
+                    onChange={handleChangeForm(target)}
                 />
                 <button className="btn" type="submit">
                     Add Contact
@@ -72,3 +82,8 @@ export function ContactForm () {
         );
     }
 }
+
+ContactForm.propTypes = {
+    onAdd: PropTypes.func.isRequired,
+//     onCheckUnique: PropTypes.func.isRequired,
+    };
